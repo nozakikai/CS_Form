dc335695960fe6471e506c4a338325d869c7e9e6
MSI\kai08 <zeal24360040@fsgcl.jp>
2024-11-01 15:04:26 +0900

    ボタンの配置

--- CS_From/CS_From/Form1.cs
+++ CS_From/CS_From/Form1.cs
@@ -17,9 +17,35 @@ namespace CS_From
             InitializeComponent();
 
 
-            TestButton testButton = new TestButton();
-
+           TestButton testButton = new TestButton(400,40,100,100);
             Controls.Add(testButton);
+
+           TestButton testButton2 = new TestButton(15,15,15,15);
+            Controls.Add(testButton2);
+
+            TestButton testButton3 = new TestButton(20, 20,20, 20);
+            Controls.Add(testButton3);
+
+            TestButton testButton4 = new TestButton(30, 30, 30,30);
+            Controls.Add(testButton4);
+
+            TestButton testButton5 = new TestButton(60, 20, 30, 30);
+            Controls.Add(testButton5);
+
+            TestButton testButton6 = new TestButton(80, 20, 30, 30);
+            Controls.Add(testButton6);
+
+            TestButton testButton7 = new TestButton(100, 20, 110, 40);
+            Controls.Add(testButton7);
+
+            TestButton testButton8 = new TestButton(60, 120, 55, 40);
+            Controls.Add(testButton8);
+
+            TestButton testButton9 = new TestButton(140, 100, 50, 40);
+            Controls.Add(testButton9);
+
+            TestButton testButton10 = new TestButton(200, 120, 50, 40);
+            Controls.Add(testButton10);
         }
 
 
--- CS_From/CS_From/TestButton.cs
+++ CS_From/CS_From/TestButton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,21 @@ namespace CS_From
 {
     class TestButton:Button
     {
-        public TestButton()
+        public TestButton(int x,int y,int width ,int height)
         {
+            ///コンストラクタ
+            ///クラスを生成したときに呼び出される
             //ClickイベントにOnClick 関数を登録
             //ボタンをクリックしたときに登録した関数を実行する
             Click += OnClick;
 
+            //ボタン内に文字を表示させる
+            Text = "ボタンです";
+
+            Location = new Point(x,y);
+            { 
+                Size = new Size(width,height);
+            }
 
         }
 
